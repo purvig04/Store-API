ffd5ee5b431b8f014e2ef473e24511896529151b
purvi <purvig@id.argusoft.com>
2025-08-12 17:47:06 +0530

    add db and middleware

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMddleware = require("./middleware/error-handler");
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Home page of store api dw");
+  res.send('<h1>Store API <h1><a href="/api/v1/products">products route </a>');
 });
 
-app.listen(3000, (req, res) => {
-  console.log("Server is listening on port 3000...");
-});
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    //connect db
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

8745480e3d8433c62ea4297b3595fd8831541ab1
purvi <purvig@id.argusoft.com>
2025-08-13 15:27:38 +0530

    add filttering

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,34 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("product testing route");
-  res.status(200).json({ msg: "products testing route" });
+  const name = "accent";
+  const products = await Product.find({
+    name: { $regex: name, $options: "i" },
+  });
+  //   throw new Error("product testing route");
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObj.company = company;
+  }
+
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObj);
+
+  const products = await Product.find(queryObj);
+  //if we directly pass req.query then if we add something not acceptable then it might throw an error.
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {

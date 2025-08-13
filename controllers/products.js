d18f53cbb62fef7274f95def948c822ff292f905
purvi <purvig@id.argusoft.com>
2025-08-13 16:22:31 +0530

    add sorting

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,15 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const name = "accent";
-  const products = await Product.find({
-    name: { $regex: name, $options: "i" },
-  });
+  const products = await Product.find({});
+  console.log(products);
+
   //   throw new Error("product testing route");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObj = {};
 
   if (featured) {
@@ -26,9 +25,19 @@ const getAllProducts = async (req, res) => {
   }
   console.log(queryObj);
 
-  const products = await Product.find(queryObj);
+  let result = Product.find(queryObj);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = await result.sort(sortList);
+  } else {
+    result = await result.sort("createdAt");
+  }
+  // console.log(result);
+
+  //const products = await Product.find(queryObj);
   //if we directly pass req.query then if we add something not acceptable then it might throw an error.
-  res.status(200).json({ products, nbHits: products.length });
+  res.status(200).json({ result, nbHits: result.length });
 };
 
 module.exports = {
